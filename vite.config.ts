9040799345e9fbce42bbc8e4a6113145a6d50ea1
Ryan <kimjhs@kakao.com>
2023-05-18 14:53:16 +0900

    Initialized Project

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+import pkg from './package.json';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        formats: ['cjs', 'es'],
+        fileName: (fileName) => (fileName === 'cjs' ? 'index.js' : 'index.es.js')
+      },
+      rollupOptions: {
+        external: [...Object.keys(pkg.peerDependencies), /@emotion/g],
+        output: {
+          exports: 'named',
+          interop: 'auto'
+        }
+      }
+    },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react'
+      }),
+      dts({ insertTypesEntry: true })
+    ],
+    define: {
+      'process.env.NODE_ENV': '"production"'
+    }
+  };
+});
